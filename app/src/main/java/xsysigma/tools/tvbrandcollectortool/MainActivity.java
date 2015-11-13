fe37e304b898d2e2496f3de4eb8e78865b997032
xsysigma <xsysigma@163.com>
2015-11-13 15:03:38 +0800

    display TV brand, board, model and product

---
+++ app/src/main/java/xsysigma/tools/tvbrandcollectortool/MainActivity.java
@@ -0,0 +1,53 @@
+package xsysigma.tools.tvbrandcollectortool;
+
+import android.os.Build;
+import android.os.Bundle;
+import android.support.design.widget.FloatingActionButton;
+import android.support.design.widget.Snackbar;
+import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.Toolbar;
+import android.view.View;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.widget.TextView;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        TextView model = (TextView)findViewById(R.id.model);
+        model.setText("model="+Build.MODEL);
+        TextView product = (TextView)findViewById(R.id.product);
+        product.setText("product="+Build.PRODUCT);
+        TextView board = (TextView)findViewById(R.id.board);
+        board.setText("board="+Build.BOARD);
+        TextView brand = (TextView)findViewById(R.id.brand);
+        brand.setText("brand="+Build.BRAND);
+
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        getMenuInflater().inflate(R.menu.menu_main, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        // Handle action bar item clicks here. The action bar will
+        // automatically handle clicks on the Home/Up button, so long
+        // as you specify a parent activity in AndroidManifest.xml.
+        int id = item.getItemId();
+
+        //noinspection SimplifiableIfStatement
+        if (id == R.id.action_settings) {
+            return true;
+        }
+
+        return super.onOptionsItemSelected(item);
+    }
+}
